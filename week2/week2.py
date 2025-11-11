ac25872952134d0ec3d21c8d966ebf4596e1964c
gis-14334338 <guoyiwei0503@gmail.com>
2025-11-11 11:45:54 +0000

    Update week2.py

--- week2/week2.py
+++ week2/week2.py
@@ -4,14 +4,27 @@ from geopandas import read_file, GeoSeries
 world = read_file("C:/Users/ROG/Desktop/understandinggis/data/natural-earth/ne_10m_admin_0_countries.shp")
 
 usa = world.loc[(world.ISO_A3 == 'USA')]
-
-print(type(usa))
-
 usa_col = usa.geometry
-
-print(type(usa_col))
-
-# extract the geometry objects themselves from the GeoSeries
 usa_geom = usa_col.iloc[0]
 
-print(type(usa_geom))
+mex = world.loc[(world.ISO_A3 == 'MEX')]
+mex_col = mex.geometry
+mex_geom = mex_col.iloc[0]
+
+# calculate the intersection of the geometry objects
+border = usa_geom.intersection(mex_geom)
+
+from matplotlib.pyplot import subplots, savefig, title
+# create map axis object
+my_fig, my_ax = subplots(1, 1, figsize=(16, 10))
+
+# remove axes
+my_ax.axis('off')
+
+# plot the border
+GeoSeries(border).plot(
+  ax = my_ax
+	)
+
+# save the image
+savefig('./out/first-border.png')
